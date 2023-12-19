bebcd9e4617f618ce5a6dc70aaac7dc252b2a140
Casey Carnaghi <ccarnaghi@concordservicing.com>
2023-12-18 22:59:04 -0700

    added 3 second sleep inbetween requests

--- like_if_your_team_won_gami.py
+++ like_if_your_team_won_gami.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+import sys
 
 
 def get_nfl_scores(group_chat_teams):
@@ -8,16 +10,26 @@ def get_nfl_scores(group_chat_teams):
         response = requests.get(url)
         winning_teams = []
 
+        print(f"Week {week} winners:\n")
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
             winners = soup.find_all("tr", class_="winner")
 
             for winner in winners:
                 team_name = winner.find('td').find('a').text
+
                 if team_name in group_chat_teams:
                     winning_teams.append(team_name)
+                    print(team_name)
 
-        print(winning_teams)
+        else:
+            print(f"Could not access {url}. Reponse: {response}")
+            sys.exit()
+
+        print('***********************\n')
+        # sleep for 3.5 seconds to abide by sportsreferences
+        # guidelines of no more than 20 request per minute
+        time.sleep(3.5)
 
 
 if __name__ == "__main__":
@@ -31,5 +43,4 @@ if __name__ == "__main__":
         'Green Bay Packers': 'Dylan',
         'Los Angeles Chargers': 'Stephen & Jake'
     }
-
     get_nfl_scores(group_chat_teams)
